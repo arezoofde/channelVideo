9822de929bc696483cffcf5671d87720949d75ce
ArezooFadaei <arezoo.fde1999@gmail.com>
2024-08-17 19:27:54 +0330

    000-01-1

---
+++ src/components/Videos.jsx
@@ -0,0 +1,20 @@
+import { Stack, Box } from '@mui/material';
+import React from 'react';
+import { VideoCard, ChannelCard } from './';
+
+const Videos = ( {videos} ) => {
+  console.log(videos)
+  return (
+    <Stack direction="row" spacing={2}  flexWrap="wrap">
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard chanelDetail={item} />}
+          {item.id.channelId && <ChannelCard chanelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  )
+}
+
+export default Videos
