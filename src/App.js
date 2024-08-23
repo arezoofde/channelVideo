d9128a5412e9217458a8886148a81a2f8cfb688f
ArezooFadaei <arezoo.fde1999@gmail.com>
2024-08-23 18:57:29 +0330

    000-01-1.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
-import {Navbar , Feed ,ChannelDetail,VideoDetail,SearchFeed } from './components';
+import { Navbar, Feed, ChannelDetail, VideoDetail, SearchFeed } from './components';
 
 const App = () => (
     <BrowserRouter>
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelDetail } from './';
+import { fetchFromAPI } from "../utils/fechFromApi";
+
 
 const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([])
+  const { id } = useParams();
+  console.log(channelDetail);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part="snippet&order=data`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
